421017374a5febf238932d316ddfeb52e79dd202
DiamondDev <fynndiamond@gmail.com>
2023-06-29 18:22:00 +0100

    update script, add gitignore

--- code/bulk_build_json.py
+++ code/bulk_build_json.py
@@ -1,4 +1,5 @@
 import random as r
+import os
 
 def getIndex(list):
     return r.randint(0, len(list))
@@ -11,23 +12,45 @@ def buildList(filepath, count):
         for line in f:
             lines.append(line)
 
-        for x in range(count):
-            names.append(lines[getIndex(lines)])
+        i = count
+        while i > 0:
+            x = getIndex(lines)
+            if (lines[x] not in names):
+                names.append(lines[x])
+                i -= 1
+                print(f"[filepath={filepath}] Obtained {count-i}/{count}")
             
     string = ""
+    i = len(names)
     for name in names:
         n = name.split('\n')[0]
-        string += f"\t\"{n}\"," + "\n\t"
+        comma = ""
+        if (i > 1): comma = ","
+        string += f"\t\"{n}\"" + comma + "\n\t"
+        i -= 1
     return string
+
+def prepFile(path):
+    if (os.path.exists(path)):
+        return
+    else:
+        dirs = path.split("/")
+        os.makedirs(path[:len(dirs[len(dirs)-1])+1])
+                
+        open(path, "x")
 
 
 forePath = "./names/forenames.txt"
 mPath = "./names/middle-names.txt"
 surPath = "./names/surnames.txt"
 
+outpath = "./out/generated_names.json"
+
 forenames = ""
 mnames = ""
 surnames = ""
+
+## main
 
 foreCount = int(input("Enter count of forenames to generate: ")) # im not input validating this, cant be bothered
 mCount = int(input("Enter count of middle-names to generate: "))
@@ -38,9 +61,13 @@ mnames = buildList(mPath, mCount)
 surnames = buildList(surPath, surCount)
 
 
-
 string = "{\n\t" + f"\"forenames\": [\n\t{forenames}],\n\t\"middlenames\": [\n\t{mnames}],\n\t\"surnames\": [\n\t{surnames}]" + "\n}"
 
+prepFile(outpath)
+
 print("\n\n")
-print(string)
-    
+
+with open(outpath, 'w') as f:
+    f.write(string)
+
+print(f"Written to {outpath}!")
